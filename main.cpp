eb88cb5bbf108e9b35c5943af67619a12b40a576
Jian-Lun Chen <allen-jl@Jian-Luns-MacBook-Air.local>
2024-10-01 12:25:58 +0800

    google coding style

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
-int main(){
-    
-    cout << "GGGG" << endl;
+class Solution {
+ public:
+  bool canArrange(vector<int>& arr, int k) {
+    for (int index = 0; index < arr.size(); index++) {
+      arr[index] = (arr[index] % k + k) % k;
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int start = 0, end = arr.size() - 1;
+    while (start < end) {
+      if (arr[start] % k != 0) break;
+      if (arr[start + 1] % k != 0) return false;
+      start += 2;
+    }
+    while (start < end) {
+      if ((arr[start] + arr[end]) % k != 0) return false;
+      start++;
+      end--;
+    }
+    return true;
+  }
+};
+
+int main() {
+  int retVal = (-4) % 5;
+
+  cout << retVal << endl;
+  cout << "GGGG" << endl;
 }
